b577b6448a265a86d7cdc022a1a7da7da10c9bc0
whypakhom <yegor46pakhomov@gmail.com>
2026-05-02 00:09:35 +0200

    Add extra piece table tests

--- piecetable/piecetable_test.go
+++ piecetable/piecetable_test.go
@@ -5,21 +5,120 @@ import (
 	"testing"
 )
 
-func TestAddToTheEnd(t *testing.T) {
-	text := "Hello\nWorld"
-	pt := InitPieceTable(text)
+func TestInsertChar(t *testing.T) {
+	t.Run("Insert at the end of an add piece (appending)", func(t *testing.T) {
+		pt := InitPieceTable("Hello")
+		// Manually create an add piece to test append
+		pt.lines[0].add = []rune("!")
+		pt.lines[0].pieces = append(pt.lines[0].pieces, &PieceTableRecord{false, 0, 1})
 
-	pt.InsertChar(0, 5, ' ')
-	pt.InsertChar(0, 6, 't')
-	pt.InsertChar(0, 7, 'h')
-	pt.InsertChar(0, 8, 'i')
-	pt.InsertChar(0, 9, 's')
-	pt.InsertChar(0, 10, ' ')
-	line := string(pt.GetLines(0, 1)[0])
-	expected := "Hello this "
-	if line != expected {
-		t.Errorf("Expected '%v', got '%v'", expected, line)
-	}
+		pt.InsertChar(0, 6, 'X')
+		expected := "Hello!X"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("Insert in the middle of an add piece", func(t *testing.T) {
+		pt := InitPieceTable("Hello")
+		// Manually create an add piece
+		pt.lines[0].add = []rune("ab")
+		pt.lines[0].pieces = append(pt.lines[0].pieces, &PieceTableRecord{false, 0, 2})
+
+		pt.InsertChar(0, 6, 'X')
+
+		expected := "HelloaXb"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("Insert in the middle of an orig piece", func(t *testing.T) {
+		pt := InitPieceTable("Hello")
+		pt.InsertChar(0, 2, 'X')
+		expected := "HeXllo"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("Insert in the middle of orig piece - append char to add", func(t *testing.T) {
+		pt := InitPieceTable("Hello World!")
+		// Simulate state: "Hello nice World!"
+		pt.lines[0].add = []rune(" nice")
+		pt.lines[0].pieces = []*PieceTableRecord{
+			{true, 0, 5},
+			{false, 0, 5},
+			{true, 5, 7},
+		}
+
+		pt.InsertChar(0, 16, 'X')
+		expected := "Hello nice WorldX!"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("Insert in the middle of orig piece - next add pieces should move", func(t *testing.T) {
+		pt := InitPieceTable("Hello World!")
+		// Simulate state: "Hello nice World! Bye!"
+		pt.lines[0].add = []rune(" nice Bye!")
+		pt.lines[0].pieces = []*PieceTableRecord{
+			{true, 0, 5},
+			{false, 0, 5},
+			{true, 5, 7},
+			{false, 5, 5},
+		}
+
+		pt.InsertChar(0, 13, 'X')
+		expected := "Hello nice WoXrld! Bye!"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("Insert in the middle of add piece - next add pieces should move", func(t *testing.T) {
+		pt := InitPieceTable("Hello World!")
+		// Simulate state: "Hello nice World! Bye!"
+		pt.lines[0].add = []rune(" nice Bye!")
+		pt.lines[0].pieces = []*PieceTableRecord{
+			{true, 0, 5},
+			{false, 0, 5},
+			{true, 5, 7},
+			{false, 5, 5},
+		}
+
+		pt.InsertChar(0, 8, 'X')
+		expected := "Hello niXce World! Bye!"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("Append to the end of add piece - next add pieces should move", func(t *testing.T) {
+		pt := InitPieceTable("Hello World!")
+		// Simulate state: "Hello nice World! Bye!"
+		pt.lines[0].add = []rune(" nice Bye!")
+		pt.lines[0].pieces = []*PieceTableRecord{
+			{true, 0, 5},
+			{false, 0, 5},
+			{true, 5, 7},
+			{false, 5, 5},
+		}
+
+		pt.InsertChar(0, 10, 'X')
+		expected := "Hello niceX World! Bye!"
+		actual := string(pt.GetLines(0, 1)[0])
+		if actual != expected {
+			t.Errorf("Expected '%s', got '%s'", expected, actual)
+		}
+	})
 }
 
 func TestInitPieceTable(t *testing.T) {
